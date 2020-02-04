071374c1014f67456bf82c6339480f587ef7f15f
BlackShark <lea2616@gmail.com>
2020-02-04 19:10:41 +0500

    initial changes

--- IrcCommands/Discord.py
+++ IrcCommands/Discord.py
@@ -1,5 +1,5 @@
 #Called when pm'd with
 #	!discord
 def run(user, msg, ircClient, conf, api, time):
-    ircClient.msg(user, 'The place to talk about the bot or just chat: https://discord.gg/hKXQdm2')
+    ircClient.msg(user, 'The place to talk about the bot or just chat: https://discord.gg/TN43w3E')
     print(f'{time} Printed discord invite for {user}.')
--- IrcCommands/Help.py
+++ IrcCommands/Help.py
@@ -1,5 +1,5 @@
 #Called when pm'd with
 #	!help
 def run(user, msg, ircClient, conf, api, time):
-    ircClient.msg(user, 'A list of commands can be found in the [GitHub wiki](https://github.com/SarahIsWeird/taiko-bot/wiki/Commands).')
+    ircClient.msg(user, 'WIP')
     print(f'{time} Printed command list for {user}.')
